f84ab536f3bd7a5a0ec29e1ac1eaf60fd4271380
davideichler <80835619+davideichler@users.noreply.github.com>
2023-01-16 21:50:36 +0100

    Anfang

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", hndLoad)
+
+function hndLoad() {
+  /*   window.onscroll = function() {stickToTop()};
+
+    var header = document.querySelector(".header");
+    var sticky = header.offsetTop;
+    console.log(sticky)
+    console.log(scrollY)
+    
+    function stickToTop() {
+        if (scrollY > sticky) {
+            header.classList.add("sticky");
+        }   else {
+            header.classList.remove("sticky");
+        }
+    } */
+}
